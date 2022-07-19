218ceb2267fc02c067b37093a4d46a187aeac387
Tuan <phamdinhtuan1912@gmail.com>
2022-07-19 18:50:10 +0700

    add code asm2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import AddTaskForm from './component/AddTaskForm';
+import TaskList from './component/TaskList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+    };
+    this.deleteItem = this.deleteItem.bind(this)
+  }
+
+  handleNameChange = (e) => {
+    this.setState({
+      nameText: e.target.value
+    })
+  }
+
+  handleDateChange = (e) => {
+    this.setState({
+      dateValue: e.target.value
+    })
+  }
+
+  handlePriorityChange = (e) => {
+    this.setState({
+      priorityValue: e.target.value
+    }
+    )
+  }
+
+  onSubmit = () => {
+    this.setState({
+      items: [
+        ...this.state.items, {
+          'name': this.state.nameText,
+          'date': this.state.dateValue,
+          'priority': this.state.priorityValue
+        }
+      ]
+    })
+  }
+
+  deleteItem(name) {
+    const filteredItems = this.state.items.filter(item =>
+      item.name !== name);
+    this.setState({
+      items: filteredItems
+
+    })
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <AddTaskForm
+          taskName={this.handleNameChange}
+          taskDeadline={this.handleDateChange}
+          taskPriority={this.handlePriorityChange}
+          onSubmit={this.onSubmit}
+        />
+        <div>
+          <h1>List task</h1>
+          {this.state.items.map(item => {
+            return (
+              <TaskList
+                data={item}
+                deleteItem={this.deleteItem}
+              />
+            )
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
